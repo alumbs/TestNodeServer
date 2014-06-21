bbff18f322985f8829b6cbc4ffce7529c4c20c1b
Chibuzor Alumba <coolchibs@gmail.com>
2014-06-21 09:36:58 -0500

    Modified the server to receive multiple socket connections. Modified the server to emit a message to all sockets connected. Modified the client to emit a message to the server socket as opposed to sending a get request.

--- server/server.js
+++ server/server.js
@@ -4,29 +4,22 @@ var restify = require('restify');
 var ecstatic = require('ecstatic');
 
 var server = restify.createServer();
-var io = require('socket.io');
-var socket;
+var io = require('socket.io').listen(server);
+//var socket;
 server.pre(ecstatic({ root: __dirname + '/public'}));
 server.use(restify.queryParser());
 
 server.get("/initialize", function(request, response, next){
-	
+	/*
 	if(socket)
 	{
 		console.log('initialize request received');
-		//send to all sockets but the new one
-		//socket.broadcast.emit('news', { socket: 'initialized' });
 		
-		//send only to the newly created socket
-		//socket.emit('news', {socket: 'initialized'});
-		
-		//send to all sockets
-		io.sockets.emit('news', { socket: 'initialized' });
 	}
 	else
 	{
 		console.log('socket not initialized');
-	}
+	}*/
 	response.writeHead(200, {});
 	response.write('I love you baby!');
 	response.end();
@@ -52,17 +45,34 @@ server.get("/katherinebaby", function(request, response, next){
 
 server.listen(8080);
 
-io.listen(server).on('connection', function(sock){
+//open the socket to listen on port 8080
+//io.listen(8080);
+
+//whenever a socket connects...
+io.sockets.on('connection', function(sock){
 	console.log('socket connected');
 	if(sock)
 	{
-		socket = sock;
-		sock.emit('news', { hello: 'world' });
-		sock.on('my other event', function(data)
+		//socket = sock;
+		sock.emit('news', { news: 'hello world' });
+		/*sock.on('my other event', function(data)
 		{	
 			console.log(data);
+		});*/
+		sock.on('sendMessageToAllClient', function(data)
+		{
+			console.log('reeived message for all clients');
+			io.sockets.emit('news', { news: data['message'] });
 		});
 	}	
+	//send to all sockets but the new one
+		//socket.broadcast.emit('news', { socket: 'initialized' });
+		
+		//send only to the newly created socket
+		//socket.emit('news', {socket: 'initialized'});
+		
+		//send to all sockets
+		//io.sockets.emit('news', { news: 'start a game in 5 seconds' });
 });
 
 console.log('Server started and listening');
